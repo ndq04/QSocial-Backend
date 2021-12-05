d378ef795507df3a79faa453065c016da5d55975
ndq04 <duyquang412000@gmail.com>
2021-12-05 10:41:05 +0700

    create updateUpdateAvatar router

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,6 +24,37 @@ const userController = {
       return res.status(500).json({message: error.message})
     }
   },
+
+  updateUserProfile: async (req, res) => {
+    try {
+      const {firstname, lastname, livein, from, job} = req.body
+      if (!firstname) {
+        return res.status(400).json({message: 'Firstname is requires'})
+      }
+      if (!lastname) {
+        return res.status(400).json({message: 'Lastname is requires'})
+      }
+
+      await User.findOneAndUpdate(
+        {_id: req.user._id},
+        {firstname, lastname, livein, from, job}
+      )
+
+      res.status(200).json({message: 'Cập nhật thông tin thành công'})
+    } catch (error) {
+      return res.status(500).json({message: error.message})
+    }
+  },
+
+  updateUserAvatar: async (req, res) => {
+    try {
+      await User.findOneAndUpdate({_id: req.user._id}, req.body)
+
+      res.status(200).json({message: 'Cập nhật ảnh đại diện thành công'})
+    } catch (error) {
+      return res.status(500).json({message: error.message})
+    }
+  },
 }
 
 module.exports = userController
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,11 +1,22 @@
 const router = require('express').Router()
 const auth = require('../middlewares/auth')
-const {searchUser, getUser} = require('../controllers/userController')
+const {
+  searchUser,
+  getUser,
+  updateUserProfile,
+  updateUserAvatar,
+} = require('../controllers/userController')
 
 //SEARCH USERS
 router.get('/search', auth, searchUser)
 
 //GET A USER
 router.get('/user/:id', auth, getUser)
+
+// UPDATE A USER PROFILE
+router.patch('/userprofile', auth, updateUserProfile)
+
+// UPDATE A USER AVATAR
+router.patch('/useravatar', auth, updateUserAvatar)
 
 module.exports = router
